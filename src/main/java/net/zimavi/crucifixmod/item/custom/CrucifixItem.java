898ac86f720503e8a335476e624af3d78b192ab7
ZImaVI <72853841+zimavi@users.noreply.github.com>
2023-02-14 20:59:48 +0200

    Added animation of Crucifix

--- src/main/java/net/zimavi/crucifixmod/item/custom/CrucifixItem.java
+++ src/main/java/net/zimavi/crucifixmod/item/custom/CrucifixItem.java
@@ -1,12 +1,14 @@
 package net.zimavi.crucifixmod.item.custom;
 
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
 import net.minecraft.world.entity.monster.warden.Warden;
@@ -15,8 +17,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.zimavi.crucifixmod.sound.ModSounds;
 import org.jetbrains.annotations.Nullable;
+import org.openjdk.nashorn.internal.runtime.Debug;
 
 import java.util.List;
 
@@ -25,6 +29,15 @@ public class CrucifixItem extends Item {
         super(properties);
     }
 
+    private Entity addChainsEntity(double x, double y, double z, Level level){
+        CompoundTag compoundTag = new CompoundTag();
+        compoundTag.putString("id", "crucifixmod:chains");
+
+        return EntityType.loadEntityRecursive(compoundTag, level, (e) -> {
+            e.moveTo(x, y, z, 0f, 0f);
+            return e;
+        });
+    }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
@@ -38,6 +51,23 @@ public class CrucifixItem extends Item {
         super.appendHoverText(stack, level, components, flag);
     }
 
+
+    private void crucifixTimerKill(Entity entity, Entity chains) {
+        long time = chains.getLevel().getGameTime();
+        long curr = time;
+        while (time - curr != 5 * 20) {
+            curr = chains.getLevel().getGameTime();
+        }
+        entity.hurt(new DamageSource("crucifix"), 1000000000);
+
+        curr = time;
+        while (time - curr != 8 * 20) {
+            curr = chains.getLevel().getGameTime();
+        }
+        chains.kill();
+    }
+
+
     @Override
     public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, InteractionHand hand) {
         //Item change
@@ -46,13 +76,24 @@ public class CrucifixItem extends Item {
         if(entity instanceof Warden == false && entity instanceof EnderDragon == false && entity instanceof WitherBoss == false && entity instanceof Player == false) {
             player.getLevel().playSound(player, player.position().x, player.position().y, player.position().z,
                     ModSounds.CRUCIFIX_USE.get(), SoundSource.PLAYERS, 1f, 1f);
-            entity.hurt(new DamageSource("crucifix"), 1000000000);
+            //entity.hurt(new DamageSource("crucifix"), 1000000000);
+            Vec3 pos = new Vec3(0f, 0.2f, 0f);
+            entity.move(MoverType.SELF, pos);
+            entity.setNoGravity(true);
+            entity.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0D);
+            Entity chains = addChainsEntity(
+                    entity.position().x,
+                    entity.position().y,
+                    entity.position().z,
+                    entity.getLevel());
+            player.getLevel().addFreshEntity(chains);
+            crucifixTimerKill(entity, chains);
         }
         else if (entity instanceof Player
         ) {
             player.getLevel().playSound(player, player.position().x, player.position().y, player.position().z,
                     ModSounds.CRUCIFIX_USE.get(), SoundSource.PLAYERS, 1f, 1f);
-            entity.hurt(new DamageSource("crucifix"), 1000000000);
+            //entity.hurt(new DamageSource("crucifix"), 1000000000);
         }
         else {
             player.getLevel().playSound(player, player.position().x, player.position().y, player.position().z,
