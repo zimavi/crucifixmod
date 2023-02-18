94e1fd40092dd0847b2c3d58f9e38a3b63a0f266
ZImaVI <72853841+zimavi@users.noreply.github.com>
2023-02-18 19:49:42 +0200

    Added records from DOORS. (untested)

--- src/main/java/net/zimavi/crucifixmod/item/ModItems.java
+++ src/main/java/net/zimavi/crucifixmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.zimavi.crucifixmod.item;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.RecordItem;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,6 +12,7 @@ import net.zimavi.crucifixmod.CrucifixMod;
 import net.zimavi.crucifixmod.entity.ModEntities;
 import net.zimavi.crucifixmod.item.custom.CrucifixItem;
 import net.zimavi.crucifixmod.item.custom.VitaminsItem;
+import net.zimavi.crucifixmod.sound.ModSounds;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -29,6 +31,31 @@ public class ModItems {
                     .stacksTo(16)
                     .setNoRepair()
                     .rarity(Rarity.UNCOMMON)));
+
+    public static final RegistryObject<Item> DAWN_OF_THE_DOORS_RECORD = ITEMS.register("dawn_of_the_doors_record",
+            () -> new RecordItem(1, ModSounds.MUSIC_DAWN_OF_THE_DOORS.get(), new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.RARE), 4680));
+    public static final RegistryObject<Item> ELEVATOR_JAM_RECORD = ITEMS.register("elevator_jam_record",
+            () -> new RecordItem(2, ModSounds.MUSIC_ELEVATOR_JAM.get(), new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.RARE), 620));
+    public static final RegistryObject<Item> GUIDING_LIGHT_RECORD = ITEMS.register("guiding_light_record",
+            () -> new RecordItem(3, ModSounds.MUSIC_GUIDING_LIGHT.get(), new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.RARE), 1200));
+    public static final RegistryObject<Item> HERE_I_COME_RECORD = ITEMS.register("here_i_come_record",
+            () -> new RecordItem(4, ModSounds.MUSIC_HERE_I_COME.get(), new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.RARE), 2220));
+    public static final RegistryObject<Item> JEFFS_JINGLE_RECORD = ITEMS.register("jeffs_jingle_record",
+            () -> new RecordItem(5, ModSounds.MUSIC_JEFFS_JINGLE.get(), new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.RARE), 2500));
+    public static final RegistryObject<Item> UNHINGED_RECORD = ITEMS.register("unhinged_record",
+            () -> new RecordItem(6, ModSounds.MUSIC_UNHINGED.get(), new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.RARE), 3820));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/net/zimavi/crucifixmod/sound/ModSounds.java
+++ src/main/java/net/zimavi/crucifixmod/sound/ModSounds.java
@@ -22,8 +22,21 @@ public class ModSounds{
     public static final RegistryObject<SoundEvent> CRUCIFIX_REANIMATION =
             registerSoundEvent("crucifix_reanimation");
 
+    //music segment
+
+    public static final RegistryObject<SoundEvent> MUSIC_DAWN_OF_THE_DOORS = registerMusicSoundEvent("music_dawn_of_the_doors");
+    public static final RegistryObject<SoundEvent> MUSIC_ELEVATOR_JAM = registerMusicSoundEvent("music_elevator_jam");
+    public static final RegistryObject<SoundEvent> MUSIC_GUIDING_LIGHT = registerMusicSoundEvent("music_guiding_light");
+    public static final RegistryObject<SoundEvent> MUSIC_HERE_I_COME = registerMusicSoundEvent("music_here_i_come");
+    public static final RegistryObject<SoundEvent> MUSIC_JEFFS_JINGLE = registerMusicSoundEvent("music_jeffs_jingle");
+    public static final RegistryObject<SoundEvent> MUSIC_UNHINGED = registerMusicSoundEvent("music_unhinged");
+
     private static RegistryObject<SoundEvent> registerSoundEvent(String name){
         return SOUND_EVENTS.register(name, () -> SoundEvent.createFixedRangeEvent(new ResourceLocation(CrucifixMod.MOD_ID, name), 15));
+    }
+
+    private static RegistryObject<SoundEvent> registerMusicSoundEvent(String name){
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(CrucifixMod.MOD_ID, name)));
     }
 
     public static void register(IEventBus eventBus){
